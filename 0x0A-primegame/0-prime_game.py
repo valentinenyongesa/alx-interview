2d1459aa1b8b51052c270a16fc588b827fd6bf04
valentinenyongesa <valentinenyongesa94@gmail.com>
2024-05-14 03:38:14 -0700

    Determine the winner of a prime game

--- 0x0A-primegame/0-prime_game.py
+++ 0x0A-primegame/0-prime_game.py
@@ -8,7 +8,6 @@ def is_prime(num):
             return False
     return True
 
-
 def generate_primes(limit):
     primes = []
     for num in range(2, limit + 1):
@@ -16,32 +15,28 @@ def generate_primes(limit):
             primes.append(num)
     return primes
 
-
 def play_round(primes, n):
     if n <= 1:
         return None  # No prime numbers to choose
     for prime in primes:
-        if prime <= n:
-            n -= n // prime * prime
-            if n == 0:
-                return "Maria"
-    return "Ben"
-
+        if prime <= n and n % prime == 0:
+            return "Ben"
+    return "Maria"
 
 def isWinner(x, nums):
     maria_wins = 0
+    ben_wins = 0
     primes = generate_primes(max(nums))
     for n in nums:
         winner = play_round(primes, n)
         if winner == "Maria":
             maria_wins += 1
-    if maria_wins > x // 2:
-        return "Maria"
-    elif maria_wins < x // 2:
+        elif winner == "Ben":
+            ben_wins += 1
+    if ben_wins > maria_wins:
         return "Ben"
     else:
-        return None
-
+        return "Maria"
 
 if __name__ == "__main__":
     print("Winner: {}".format(isWinner(5, [2, 5, 1, 4, 3])))
