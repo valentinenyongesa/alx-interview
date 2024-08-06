74e6ecfd8bf6700a5ee4dd282d7776db58fe5ff3
valentinenyongesa <valentinenyongesa94@gmail.com>
2024-08-05 23:52:05 -0700

    Algorithm of Prime game

--- 0x0A-primegame/0-prime_game.py
+++ 0x0A-primegame/0-prime_game.py
@@ -1,62 +1,50 @@
 #!/usr/bin/python3
 
 
-def is_prime(n):
-    if n <= 1:
-        return False
-    if n <= 3:
-        return True
-    if n % 2 == 0 or n % 3 == 0:
-        return False
-    i = 5
-    while i * i <= n:
-        if n % i == 0 or n % (i + 2) == 0:
-            return False
-        i += 6
-    return True
-
-
-def sieve_of_eratosthenes(n):
-    primes = []
-    sieve = [True] * (n + 1)
+def sieve_of_eratosthenes(limit):
+    """Generate a list of primes up to `limit`."""
+    is_prime = [True] * (limit + 1)
     p = 2
-    while p**2 <= n:
-        if sieve[p]:
-            primes.append(p)
-            for i in range(p**2, n + 1, p):
-                sieve[i] = False
+    while p * p <= limit:
+        if is_prime[p]:
+            for i in range(p * p, limit + 1, p):
+                is_prime[i] = False
         p += 1
-    for p in range(p, n + 1):
-        if sieve[p]:
-            primes.append(p)
-    return primes
-
-
-def simulate_round(nums):
-    primes = sieve_of_eratosthenes(max(nums))
-    for prime in primes:
-        nums = [num for num in nums if num % prime != 0 or num == prime]
-        if not nums:
-            return "Maria" if prime in nums else "Ben"
-    return "Maria"
+    return [p for p in range(2, limit + 1) if is_prime[p]]
 
 
 def isWinner(x, nums):
+    """Determine the winner of the most rounds of the Prime Game."""
+    if not nums or x <= 0:
+        return None
+
     maria_wins = 0
     ben_wins = 0
-    for i in range(x):
-        winner = simulate_round(list(range(1, nums[i] + 1)))
-        if winner == "Maria":
+
+    for n in nums:
+        primes = sieve_of_eratosthenes(n)
+        turn = 0  # Maria starts first
+        # Available numbers from 1 to n
+        available = [True] * (n + 1)
+        available[0] = False  # 0 is not part of the game
+
+        for prime in primes:
+            if prime > n:
+                break
+            if available[prime]:
+                # Remove this prime and its multiples
+                for multiple in range(prime, n + 1, prime):
+                    available[multiple] = False
+                turn = 1 - turn  # Switch turns
+
+        if turn == 0:
             maria_wins += 1
-        elif winner == "Ben":
+        else:
             ben_wins += 1
+
     if maria_wins > ben_wins:
         return "Maria"
-    elif maria_wins < ben_wins:
+    elif ben_wins > maria_wins:
         return "Ben"
     else:
         return None
-
-
-if __name__ == "__main__":
-    print("Winner: {}".format(isWinner(5, [2, 5, 1, 4, 3])))
